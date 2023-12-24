08221f8f68f083ed5c03ee708bfbf117494b8160
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2023-12-24 17:21:25 -0600

    axios added first version

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useState } from "react"; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ImageBackground, TextInput } from 'react-native';
 import axios from 'axios';
 
 
 
 export default function App() { 
   const [advice, setAdvice] = useState(""); 
+  const[spriteBack, setBack] = useState("");
+  const[spriteFront, setFront] = useState("");
 
   const getRandomId = (min, max) => { 
       min = Math.ceil(min); 
@@ -22,16 +24,25 @@ export default function App() {
               getRandomId(1, 900)) 
           .then((response) => { 
               setAdvice(response.data.species.name); 
-              //setAdvice(response.data.sprites.back_default);
+              setBack(response.data.sprites.back_default);
+              setFront(response.data.sprites.front_default);
           }); 
   }; 
 
   return ( 
       <View style={styles.container}> 
-          <Text style={styles.text}>{advice}</Text> 
-          <Image style={styles.Image} source={{uri: 'https://reactjs.org/logo-og.png'}}></Image>
-          <Button title="Get Advice" 
+      
+          
+          <TextInput placeholder='search pokimon' style={styles.input}></TextInput>
+          <Text style={styles.text}>{advice}</Text>
+          <Image style={styles.Image} source={{uri: spriteBack}}></Image> 
+          <Image style={styles.Image} source={{uri: spriteFront}}></Image>
+          
+          <Button title="Get Pokimon" 
               onPress={getAdvice} color="black" /> 
+
+
+              
       </View> 
   ); 
 } 
@@ -54,7 +65,6 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
-    
   },
   text:{
     fontSize: 24,
@@ -62,12 +72,19 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     marginTop: 160,
     marginLeft: 175,
-    
+  },
+  input:{
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+    marginLeft: 115,
+    marginTop: -145,
   },
   Image:{
     marginLeft: 115,
     width: 200, 
     height: 200,
+    marginBottom: 55,
   }
 });
 
